c14d7aae05eabfc5517a8d8608e223b1c6a0fdb3
GigiaJ <gigiaboone@yahoo.com>
2025-11-24 03:26:45 -0600

    Fix refs

--- src/pulumicljs/execution/stack_processor.cljs
+++ src/pulumicljs/execution/stack_processor.cljs
@@ -1,8 +1,8 @@
 (ns pulumicljs.execution.stack-processor
   (:require
    ["@pulumi/kubernetes" :as k8s]
-   ["pulumi-extra-crds/gateway" :as gateway-api]
-   ["pulumi-extra-crds/cert_manager" :as cert-manager]
+   ["pulumi-extra-crds/generated/crds/gateway" :as gateway-api]
+   ["pulumi-extra-crds/generated/crds/cert_manager" :as cert-manager]
    ["@pulumi/pulumi" :as pulumi]
    ["@pulumi/vault" :as vault]
    ["@pulumiverse/harbor" :as harbor]
