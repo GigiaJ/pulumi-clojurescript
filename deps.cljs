0c271b82d97ba4c03cc3bff06451242ad78a82f3
GigiaJ <gigiaboone@yahoo.com>
2025-11-24 02:32:47 -0600

    Add deps

---
+++ deps.cljs
@@ -0,0 +1,14 @@
+{:npm-deps
+ {"@pulumi/cloudflare" "6.9.1"
+  "@pulumi/command" "^1.1.0"
+  "@pulumi/docker" "4.9.0"
+  "@pulumi/docker-build" "^0.0.15"
+  "@pulumi/esc-sdk" "0.12.1"
+  "@pulumi/hcloud" "^1.24.0"
+  "@pulumi/kubernetes" "^4.23.0"
+  "@pulumi/pulumi" "^3.113.0"
+  "@pulumi/vault" "^7.2.1"
+  "@pulumiverse/harbor" "^3.10.21"
+  "pulumi-extra-crds" "^1.0.13"
+  "bcryptjs" "^3.0.2"
+  "uuid" "^13.0.0"}}
