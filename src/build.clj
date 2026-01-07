7e82a17d039433729fed9bfc8a185e31e8a1be88
GigiaJ <gigiaboone@yahoo.com>
2026-01-06 23:48:56 -0600

    Add Clerk build

---
+++ src/build.clj
@@ -0,0 +1,20 @@
+(ns build
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]))
+
+(defn build-site [_]
+  (println "👷 checking for notebooks...")
+  
+  (let [files ["src/notebooks/config_processor.cljc"
+               "src/notebooks/home.cljc"
+               "src/notebooks/sidebar.cljc"]
+        valid-files (filter #(.exists (io/file %)) files)]
+
+    (if (seq valid-files)
+      (do
+        (println "Building site with:" valid-files)
+        (clerk/build!
+         {:paths valid-files
+          :out-path "public"
+          :compile-css false}))
+      (println "ERROR: No valid notebook files found."))))
