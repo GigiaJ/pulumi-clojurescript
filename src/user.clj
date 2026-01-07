d29cbbe0f119da10b8962eaa2dc04089139f2413
GigiaJ <gigiaboone@yahoo.com>
2026-01-06 23:49:35 -0600

    listen on all interfaces

--- src/user.clj
+++ src/user.clj
@@ -1,7 +1,10 @@
 (ns user
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [notebooks.home]))
 
 (defn start! [_]
- (clerk/serve! {:browse? false
-                :watch-paths ["notebooks"]}))
-
+  (clerk/serve! {:browse? false
+                 :port 7777 
+                 :host "0.0.0.0"
+                 :watch-paths ["notebooks"]})
+  (clerk/show! 'notebooks.home))
