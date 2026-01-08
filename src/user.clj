53e45490d715ad2885ae63ecc855a5d848081796
GigiaJ <gigiaboone@yahoo.com>
2026-01-08 03:23:57 -0600

    trying stuff

--- src/user.clj
+++ src/user.clj
@@ -1,7 +1,9 @@
 (System/setProperty "CLERK_ENV" "dev")
 (ns user
   (:require [nextjournal.clerk :as clerk]
-            [home :as home]))
+            [home :as home]
+            [viewers :as viewers]))
+
 
 
 ^{::clerk/visibility {:code :hide :result :hide}}
@@ -9,5 +11,6 @@
   (clerk/serve! {:browse? false
                  :port 7777 
                  :host "0.0.0.0"
+                 :config {:html-head [:script {:src "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"}]}
                  :watch-paths ["notebooks"]})
   (clerk/show! 'home))
